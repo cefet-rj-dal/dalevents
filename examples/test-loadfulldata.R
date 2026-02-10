24ee678b7e27e57e28c2741faaf7c85dc6e0fbb0
Eduardo Ogasawara <eogasawara@eic.cefet-rj.br>
2026-02-10 10:47:06 -0300

    Update test-loadfulldata.R

--- examples/test-loadfulldata.R
+++ examples/test-loadfulldata.R
@@ -1,41 +1,107 @@
 #!/usr/bin/env Rscript
 
-args <- commandArgs(trailingOnly = TRUE)
-pkg <- if (length(args) >= 1) args[[1]] else "united"
+library(united)
 
-library(pkg, character.only = TRUE)
+# 1
+data(A1Benchmark)
+A1Benchmark <- loadfulldata(A1Benchmark)
 
-data_dir <- system.file("data", package = pkg)
-if (data_dir == "") {
-  stop("Package data directory not found for: ", pkg)
-}
+# 2
+data(A2Benchmark)
+A2Benchmark <- loadfulldata(A2Benchmark)
 
-files <- list.files(data_dir, pattern = "\\.RData$", full.names = FALSE)
-if (length(files) == 0) {
-  stop("No .RData files found in installed package data/.")
-}
+# 3
+data(A3Benchmark)
+A3Benchmark <- loadfulldata(A3Benchmark)
 
-failures <- list()
+# 4
+data(A4Benchmark)
+A4Benchmark <- loadfulldata(A4Benchmark)
 
-for (f in files) {
-  name <- sub("\\.RData$", "", f)
-  message("Testing: ", name)
-  ok <- tryCatch({
-    utils::data(list = name, package = pkg, envir = .GlobalEnv)
-    obj <- get(name, envir = .GlobalEnv)
-    full <- loadfulldata(obj)
-    if (is.null(full)) stop("loadfulldata returned NULL")
-    TRUE
-  }, error = function(e) {
-    failures[[name]] <<- conditionMessage(e)
-    FALSE
-  })
-  if (!ok) message("  FAILED: ", failures[[name]])
-}
+# 5
+data(gecco)
+gecco <- loadfulldata(gecco)
 
-if (length(failures) > 0) {
-  message("Failures: ", length(failures))
-  quit(status = 1)
-} else {
-  message("All datasets loaded successfully.")
-}
+# 6
+data(mit_bih_MLII)
+mit_bih_MLII <- loadfulldata(mit_bih_MLII)
+
+# 7
+data(mit_bih_V1)
+mit_bih_V1 <- loadfulldata(mit_bih_V1)
+
+# 8
+data(mit_bih_V2)
+mit_bih_V2 <- loadfulldata(mit_bih_V2)
+
+# 9
+data(mit_bih_V5)
+mit_bih_V5 <- loadfulldata(mit_bih_V5)
+
+# 10
+data(nab_artificialWithAnomaly)
+nab_artificialWithAnomaly <- loadfulldata(nab_artificialWithAnomaly)
+
+# 11
+data(nab_realAdExchange)
+nab_realAdExchange <- loadfulldata(nab_realAdExchange)
+
+# 12
+data(nab_realAWSCloudwatch)
+nab_realAWSCloudwatch <- loadfulldata(nab_realAWSCloudwatch)
+
+# 13
+data(nab_realKnownCause)
+nab_realKnownCause <- loadfulldata(nab_realKnownCause)
+
+# 14
+data(nab_realTraffic)
+nab_realTraffic <- loadfulldata(nab_realTraffic)
+
+# 15
+data(nab_realTweets)
+nab_realTweets <- loadfulldata(nab_realTweets)
+
+# 16
+data(oil_3w_Type_1)
+oil_3w_Type_1 <- loadfulldata(oil_3w_Type_1)
+
+# 17
+data(oil_3w_Type_2)
+oil_3w_Type_2 <- loadfulldata(oil_3w_Type_2)
+
+# 18
+data(oil_3w_Type_4)
+oil_3w_Type_4 <- loadfulldata(oil_3w_Type_4)
+
+# 19
+data(oil_3w_Type_5)
+oil_3w_Type_5 <- loadfulldata(oil_3w_Type_5)
+
+# 20
+data(oil_3w_Type_6)
+oil_3w_Type_6 <- loadfulldata(oil_3w_Type_6)
+
+# 21
+data(oil_3w_Type_7)
+oil_3w_Type_7 <- loadfulldata(oil_3w_Type_7)
+
+# 22
+data(oil_3w_Type_8)
+oil_3w_Type_8 <- loadfulldata(oil_3w_Type_8)
+
+# 23
+data(ucr_ecg)
+ucr_ecg <- loadfulldata(ucr_ecg)
+
+# 24
+data(ucr_int_bleeding)
+ucr_int_bleeding <- loadfulldata(ucr_int_bleeding)
+
+# 25
+data(ucr_nasa)
+ucr_nasa <- loadfulldata(ucr_nasa)
+
+# 26
+data(ucr_power_demand)
+ucr_power_demand <- loadfulldata(ucr_power_demand)
